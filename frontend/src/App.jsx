92a8978a61557a7ca7c9b8279c683a47adbafd17
Ammar Najwan <njwn28@gmail.com>
2025-08-09 11:41:00 +0800

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import '../styles/navbar.css'
+import '../index.css'
+import {Link} from 'react-router-dom'
+import logo from '../assets/PlaySlotAlign.png'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <div className='logo'>
+        <Link to='/'>
+          <img className='logo-img' src={logo} alt="Logo" />
+        </Link>
+      </div>
+      <div className='container-nav'>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About Us</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+        <div className='btn-container'>
+          <Link>Sign Up</Link>
+          <Link className='login-btn'>Log In</Link>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
