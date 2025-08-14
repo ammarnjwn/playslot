a0af1c09cedc3d841ae0658ce4e8b1f76b6f1f4b
Ammar Najwan <njwn28@gmail.com>
2025-08-14 14:31:54 +0800

    Update signup backend API and frontend UI

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,17 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const connectToDB = require('./config/db')
+const authRoutes = require('./routes/authRoutes')
+const cors = require('cors')
 
 const PORT = process.env.PORT 
 
 connectToDB()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/auth", authRoutes)
 
 app.listen(PORT, () => {
   console.log("Servers succesfully started at server", PORT)
